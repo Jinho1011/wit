fa521020a5d6cc4b19e8b69a9a0c00aee4aab64f
Jeon <gino9940@gmail.com>
2023-03-02 13:33:39 +0900

    fix: manually add path alias

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,12 +1,16 @@
 import type { GatsbyNode } from 'gatsby';
-import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+import path from 'path';
 
 export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({
   actions,
 }) => {
   actions.setWebpackConfig({
     resolve: {
-      plugins: [new TsconfigPathsPlugin()],
+      alias: {
+        '@components': path.resolve(__dirname, `src/components`),
+        '@styles': path.resolve(__dirname, `src/styles`),
+        '@static': path.resolve(__dirname, `static`),
+      },
     },
   });
 };
